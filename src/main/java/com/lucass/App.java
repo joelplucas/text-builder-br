0fc216ca49758c0aa849397afbe387592deb7cca
Joel Pinho Lucas <joelpl@gmail.com>
2014-12-01 08:52:13 -0200

    changing POM type for JAR

--- src/main/java/com/lucass/App.java
+++ src/main/java/com/lucass/App.java
@@ -4,10 +4,8 @@ package com.lucass;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("NOSSO ENDEREÇO IP = " + args[0]);
     }
 }
